85e2924999cf84af7032ef4a19920ffb55b1e202
Salvatore Alaimo <alaimos@gmail.com>
2018-01-12 22:31:00 +0100

    Completed script to resubmit all jobs.

--- www/phensim/app/Console/Commands/ResubmitAll.php
+++ www/phensim/app/Console/Commands/ResubmitAll.php
@@ -13,14 +13,14 @@ class ResubmitAll extends Command
      *
      * @var string
      */
-    protected $signature = 'resubmit:all';
+    protected $signature = 'resubmit:all {--failed}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Resubmit all simulation';
+    protected $description = 'Resubmit simulation';
 
     /**
      * Create a new command instance.
@@ -37,9 +37,20 @@ class ResubmitAll extends Command
      */
     public function handle()
     {
-        foreach (Job::all() as $job) {
+        $query = Job::query();
+        if ($this->option('failed')) {
+            $query->where('job_status', '=', Job::FAILED);
+        }
+        foreach ($query->get() as $job) {
+            /** @var \App\Models\Job $job */
             $this->info("Dispatching job \"" . $job->getJobName() . "\" (" . $job->id . ") of " . $job->user->name);
+            \Auth::login($job->user, false);
+            $job->job_status = Job::QUEUED;
+            $job->deleteJobDirectory();
+            $job->job_data = [];
+            $job->save();
             dispatch(new DispatcherJob($job->id));
+            \Auth::logout();
         }
         return 0;
     }
