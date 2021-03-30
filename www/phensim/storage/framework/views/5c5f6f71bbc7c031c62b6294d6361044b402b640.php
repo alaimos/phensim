8dc6823915032292bf0ed73c01840c1fb5f658c6
Salvatore Alaimo <alaimos@gmail.com>
2021-03-30 16:41:01 +0200

    .gitignore fix

--- www/phensim/storage/framework/views/5c5f6f71bbc7c031c62b6294d6361044b402b640.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-    <?php echo $__env->make('layouts.footers.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-</footer><?php /**PATH /var/www/html/resources/views/layouts/footers/auth.blade.php ENDPATH**/ ?>
