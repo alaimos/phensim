cfd2112826a64cc117696aea0ebfbb2903d8a5d3
Salvatore Alaimo <alaimos@gmail.com>
2021-05-19 12:35:06 +0200

    Api bugfix

--- www/phensim/resources/views/users/partials/header.blade.php
+++ www/phensim/resources/views/users/partials/header.blade.php
@@ -1,5 +1,4 @@
-<div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="background-image: url({{ Gravatar::src(auth()->user()->email, 1920) }}); background-size: cover; background-position: center top;">
-    <!-- Mask -->
+<div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="background-image: url({{ Gravatar::src(auth()->user()->email, 1920) }}); background-size: cover; background-position: center top;">    <!-- Mask -->
     <span class="mask bg-gradient-default opacity-8"></span>
     <!-- Header container -->
     <div class="container-fluid d-flex align-items-center">
